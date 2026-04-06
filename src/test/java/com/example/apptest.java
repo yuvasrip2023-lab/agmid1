93e27f53ec536919b586523c4603060032f048cf
yuvasri-p <yuvasri.p2023@vitstudent.ac.in>
2026-04-06 11:11:52 +0530

    Create apptest.java

---
+++ src/test/java/com/example/apptest.java
@@ -0,0 +1,14 @@
+package com.example.contact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+        // If application starts successfully, this test passes
+        System.out.println("Test Passed: Application Context Loaded");
+    }
+}
