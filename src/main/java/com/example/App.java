22ab980ea1d33d0979b93f1ba0c3dd6635b486ce
yuvasri-p <yuvasri.p2023@vitstudent.ac.in>
2026-04-06 11:10:26 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example.contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+        System.out.println("Contact Management System Started");
+    }
+}
